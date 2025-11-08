4d6d5b3e00d572e857c52e759856847c84d2cc51
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-08 21:17:11 +0600

    module 5 vedio 4 done! non-premitive type data

---
+++ Module-5/src/nonPrimitive.ts
@@ -0,0 +1,12 @@
+// array, object
+
+let shopingList: string[] = ["egg", "milk", "sugar"];
+
+shopingList.push("rice");
+
+let mixedArr: (number | string | boolean)[] = ["eggs", 12, "milk", 1, true];
+
+// tuple
+let coordinates: [number, number] = [20, 30];
+let couple: [string, string] = ["Husband", "Wife"];
+let nameAgeRoll: [string, number, number] = ["Meshal", 21, 1];
