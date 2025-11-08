93bf0837a11ab8a741a0d88725dd9bb8ae96afc2
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-08 21:34:10 +0600

    module 5 vedio 4 done! object, optional type, literal type, readonly type

---
+++ Module-5/dist/nonPrimitive.js
@@ -0,0 +1,19 @@
+"use strict";
+// array, object
+Object.defineProperty(exports, "__esModule", { value: true });
+let shopingList = ["egg", "milk", "sugar"];
+shopingList.push("rice");
+let mixedArr = ["eggs", 12, "milk", 1, true];
+// tuple
+let coordinates = [20, 30];
+let couple = ["Husband", "Wife"];
+let nameAgeRoll = ["Meshal", 21, 1];
+// reference type-----------------
+const user = {
+    firstName: "Syed",
+    middleName: "Mohiuddin",
+    isMarried: false,
+    lastName: "Meshal",
+};
+console.log(user);
+//# sourceMappingURL=nonPrimitive.js.map
--- Module-5/src/nonPrimitive.ts
+++ Module-5/src/nonPrimitive.ts
@@ -10,3 +10,20 @@ let mixedArr: (number | string | boolean)[] = ["eggs", 12, "milk", 1, true];
 let coordinates: [number, number] = [20, 30];
 let couple: [string, string] = ["Husband", "Wife"];
 let nameAgeRoll: [string, number, number] = ["Meshal", 21, 1];
+
+// reference type-----------------
+const user: {
+	organization: "Programming Hero Level-1-Batch-11, Level-2-Batch-6"; //? now this value never be changed
+	// value => type : literal type
+    firstName: string;
+    middleName?: string; //? optional type
+    lastName: string;
+	readonly isMarried?: boolean; // access modifier
+} = {
+	organization: "Programming Hero Level-1-Batch-11, Level-2-Batch-6",
+    firstName: "Syed",
+    middleName: "Mohiuddin",
+	isMarried: false,
+    lastName: "Meshal",
+};
+
