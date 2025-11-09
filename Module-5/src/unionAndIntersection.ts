e387c973b495dbddc384aa2d66c7be7b1bb89d2c
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-09 18:35:10 +0600

    union & intersection done

---
+++ Module-5/src/unionAndIntersection.ts
@@ -0,0 +1,38 @@
+//? Union Type |
+
+type UserRole = "admin" | "user" | "guest";
+
+const getDashboard = (role: UserRole) => {
+    if (role === "admin") {
+        return "Admin Dashboard";
+    } else if (role === "user") {
+        return "User Dashboard";
+    } else {
+        return "Guest Dashboard";
+    }
+};
+
+// console.log(getDashboard("user"));
+
+//? Intersection Type &
+
+type Employee = {
+    id: string;
+    name: string;
+    phoneNumber: string;
+};
+
+type Manager = {
+	designation: string;
+	teamSize: number;
+}
+
+type EmployeeManager = Employee & Manager;
+
+const Meshal: EmployeeManager = {
+	id: "123",
+	name: 'Syed Mohiuddin Meshal',
+	phoneNumber: "+880 1764447574",
+	designation: "",
+	teamSize: 20
+}
